62744555fc2ae96f786c0fefcbf267a922666864
StanislavKls <73436574+StanislavKls@users.noreply.github.com>
2021-03-05 11:48:56 +0500

    Edit for linter

--- src/Render.php
+++ src/Render.php
@@ -8,9 +8,11 @@ function renderPlane(array $data): string
 
     $arr = array_map(function ($item) use ($sign) {
         if ($item['status'] === 'modifed') {
-            $item['valueBefore'] = is_bool($item['valueBefore']) ? var_export($item['valueBefore'], true) : $item['valueBefore'];
-            $item['valueAfter'] = is_bool($item['valueAfter']) ? var_export($item['valueAfter'], true) : $item['valueAfter'];
-            
+            $item['valueBefore'] = is_bool($item['valueBefore']) ?
+                                    var_export($item['valueBefore'], true) : $item['valueBefore'];
+            $item['valueAfter'] = is_bool($item['valueAfter']) ?
+                                    var_export($item['valueAfter'], true) : $item['valueAfter'];
+
             return "  {$sign['deleted']} {$item['key']}: {$item['valueBefore']}\n" .
             "  {$sign['added']} {$item['key']}: {$item['valueAfter']}";
         }
