8dd4d3e24269ba17283ca48fc4e4c9bfefbf6002
StanislavKls <73436574+StanislavKls@users.noreply.github.com>
2021-03-05 11:41:42 +0500

    Added tests

--- src/Render.php
+++ src/Render.php
@@ -8,9 +8,13 @@ function renderPlane(array $data): string
 
     $arr = array_map(function ($item) use ($sign) {
         if ($item['status'] === 'modifed') {
+            $item['valueBefore'] = is_bool($item['valueBefore']) ? var_export($item['valueBefore'], true) : $item['valueBefore'];
+            $item['valueAfter'] = is_bool($item['valueAfter']) ? var_export($item['valueAfter'], true) : $item['valueAfter'];
+            
             return "  {$sign['deleted']} {$item['key']}: {$item['valueBefore']}\n" .
             "  {$sign['added']} {$item['key']}: {$item['valueAfter']}";
         }
+        $item['value'] = is_bool($item['value']) ? var_export($item['value'], true) : $item['value'];
         return "  {$sign[$item['status']]} {$item['key']}: {$item['value']}";
     }, $data);
     return "{\n" . implode("\n", $arr) . "\n}\n";
---
+++ tests/DifferTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DifferTest extends TestCase
+{
+    private function getPath($file)
+    {
+        return __DIR__ .  DIRECTORY_SEPARATOR .
+                "fixtures" . DIRECTORY_SEPARATOR . $file;
+    }
+    public function testGendiffPlane()
+    {
+        $actual = \Differ\Differ\genDiff($this->getPath("plane_1.json"), $this->getPath("plane_2.json"));
+        $expect = file_get_contents(realpath($this->getPath("plane_expected")));
+        $this->assertEquals($expect, $actual);
+    }
+}
