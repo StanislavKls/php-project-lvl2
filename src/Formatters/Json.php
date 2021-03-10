78f8e834fbc0820cc08a688683273d58c8d41ccd
StanislavKls <73436574+StanislavKls@users.noreply.github.com>
2021-03-10 11:54:27 +0500

    For test

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,8 +4,5 @@ namespace Differ\Formatters\Json;
 
 function renderToJson($data)
 {
-    if (file_put_contents('result.json', json_encode($data))) {
-        return "Data is written in 'result.json'";
-    }
-    return "Error - Data not written to file";
+    return json_encode($data);
 }
